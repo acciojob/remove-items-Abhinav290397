93aaf429c7c1881573cba758607a563278b5f3b8
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-06 19:34:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function solve() {
 	const Select = document.querySelector("select");
-	const SelectedValue = Select.value;
+	const SelectedValue = Select.value; //this will give the color of button
 	let Options = Select.children;
 	for(let i=0;i<Options.length;i++){
 		if(Options[i].value === SelectedValue){
