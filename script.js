2b011ae07a00dbad65cd4599dfd45c6e666c6033
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-06 19:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function solve() {
+	const Select = document.querySelector("select");
+	const SelectedValue = Select.value;
+	let Options = Select.children;
+	for(let i=0;i<Options.length;i++){
+		if(Options[i].value === SelectedValue){
+			Options[i].remove();
+			break;
+		}
+	}
+}
