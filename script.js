762601a803873d80a03579823ac02e5a680a0109
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-06 19:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function solve() {
 	const Select = document.querySelector("select");
 	const SelectedValue = Select.value; //this will give the color of button
-	let Options = Select.children;
+	let Options = Select.children;  //This will give all elements(options) inside the select
 	for(let i=0;i<Options.length;i++){
 		if(Options[i].value === SelectedValue){
 			Options[i].remove();
